3194ad4959354ecb3df50a784bff002201cad82c
Santiago Ariel <santiagoarielv98@gmail.com>
2024-02-22 00:00:21 -0300

    preuba

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
+import React from "react";
+
 import usePokemon from "./hooks/usePokemon";
 
 const App = () => {
+	const [pokemon, setPokemon] = React.useState<null>(null);
 	const { error, isLoaded } = usePokemon();
 
+	React.useEffect(() => {
+		fetch("https://pokeapi.co/api/v2/pokemon/1")
+			.then((res) => res.json())
+			.then((result) => {
+				setPokemon(result);
+			});
+	}, []);
+
 	return (
-		<div>
+		<div className="bg-water">
+			{pokemon}
 			{error ? <p>{error.message}</p> : null}
 			{isLoaded ? (
 				<div>
-					<div className="max-w-sm w-full border border-red-500 rounded-xl">asd</div>
+					<div className="w-full max-w-sm  rounded-xl border  bg-water/50">asd</div>
 				</div>
 			) : (
 				<p>Loading...</p>
