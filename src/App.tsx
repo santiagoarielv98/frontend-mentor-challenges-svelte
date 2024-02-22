01a1afa18d79d8c427a31678ab8ff79eed5cef2f
Santiago Ariel <santiagoarielv98@gmail.com>
2024-02-22 00:01:25 -0300

    restaurando

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
-import React from "react";
-
 import usePokemon from "./hooks/usePokemon";
 
 const App = () => {
-	const [pokemon, setPokemon] = React.useState<null>(null);
 	const { error, isLoaded } = usePokemon();
-
-	React.useEffect(() => {
-		fetch("https://pokeapi.co/api/v2/pokemon/1")
-			.then((res) => res.json())
-			.then((result) => {
-				setPokemon(result);
-			});
-	}, []);
 
 	return (
 		<div className="bg-water">
-			{pokemon}
 			{error ? <p>{error.message}</p> : null}
 			{isLoaded ? (
 				<div>
