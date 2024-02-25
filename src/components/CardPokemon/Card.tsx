b4d6be6d8636b4ed927f43bc5321289042b1aea0
Santiago Ariel <santiagoarielv98@gmail.com>
2024-02-25 20:49:12 -0300

    cambio de colores

--- src/components/CardPokemon/Card.tsx
+++ src/components/CardPokemon/Card.tsx
@@ -19,7 +19,7 @@ export const Card = ({ pokemon }: CardProps) => {
 				"relative -z-10 w-full max-w-xs gap-4 space-y-8 overflow-hidden rounded-xl border-2 p-1 pb-8",
 				"border-gray-100 bg-gradient-to-t dark:border-neutral-700 dark:from-neutral-900 dark:to-neutral-800",
 				types.length === 2
-					? `border-b-${types[0]} border-l-${types[1]} border-r-${types[0]} border-t-${types[1]} dark:border-b-${types[0]} dark:border-l-${types[1]} dark:border-r-${types[0]} dark:border-t-${types[1]}`
+					? `border-b-${types[1]} border-l-${types[0]} border-r-${types[1]} border-t-${types[0]} dark:border-b-${types[1]} dark:border-l-${types[0]} dark:border-r-${types[1]} dark:border-t-${types[0]}`
 					: `border-${types[0]} dark:border-${types[0]}`
 			)}
 		>
